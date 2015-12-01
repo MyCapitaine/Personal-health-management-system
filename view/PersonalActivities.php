f4335d2c222ba6d67ecddcbb38706b3c3c091002
MyCapitaine <1051943988@qq.com>
2015-12-01 14:52:16 +0800

    personal activities view

---
+++ view/PersonalActivities.php
@@ -0,0 +1,21 @@
+<?php 
+	$partName = 'PersonalActivities';
+	require('./part/head.inc'); 
+?>
+
+	<body>
+    	<h1><a href="#">MainPage></a>PersonalMsg</h1>
+        <h2>created activities</h2>
+    	<h2>participanted activities</h2>
+    	<h2>join an activity</h2>
+    	<form>
+        
+        </form>
+    	<h2>create an activity</h2>
+    	<form>
+        
+        </form>
+        
+    </body>
+    
+</html>
